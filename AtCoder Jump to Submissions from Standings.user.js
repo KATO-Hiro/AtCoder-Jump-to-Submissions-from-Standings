42c2d2a647549c11cc76f9990b8c75d9b2689816
Kato Hiroki <k.hiro1818@gmail.com>
2020-03-01 09:58:21 +0900

    Add header (#6)

---
+++ AtCoder Jump to Submissions from Standings.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         AtCoder Jump to Submissions from Standings
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  順位表の得点をダブルクリックすると、該当するコンテスタントの実装を見ることができます。
+// @match        https://atcoder.jp/contests/*/standings*
+// @author       hiro_hiro
+// @downloadURL  https://github.com/KATO-Hiro/AtCoder-Jump-to-Submissions-from-Standings/atcoder_jump_to_submissions_from_standings.js
+// @updateURL    https://github.com/KATO-Hiro/AtCoder-Jump-to-Submissions-from-Standings/atcoder_jump_to_submissions_from_standings.js
+// @supportURL   https://github.com/KATO-Hiro/AtCoder-Jump-to-Submissions-from-Standings/issues
+// @grant        none
+// ==/UserScript==
