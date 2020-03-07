47b8e77996dcbba54a1cf3f857a6f82a63e1fd43
Kato Hiroki <k.hiro1818@gmail.com>
2020-03-07 17:44:21 +0900

    Enable to fetch user_name and task_id (#8)

--- AtCoder Jump to Submissions from Standings.user.js
+++ AtCoder Jump to Submissions from Standings.user.js
@@ -15,6 +15,7 @@
 $(function() {
     'use strict';
 
+    // FIXME: 肥大化してきたため、リファクタリングが必要
     $(document).on('dblclick', '.standings-result', function() {
         const $includingVirutal = $('body').find('script:contains("isVirtual =  true")')[0]
         let $prefix = '';
@@ -22,6 +23,33 @@ $(function() {
         if ($includingVirutal) {
             $prefix = '../';
         }
+
+        let $clickedRow = $(this)[0].cellIndex;
+
+        // コンテスト当日の順位表とバーチャル順位表の列の並びに違いがある
+        // 当日の順位表の並びに合わせる
+        if ($includingVirutal) {
+            $clickedRow -= 1
+        }
+
+        // 順位とユーザ名の欄を扱わずに済むようにインデックスを補正
+        // A問題がindex = 0となるようにしている
+        $clickedRow -= 3
+
+        // TODO: 順位表の範囲外(0未満 or 問題数 + 1以上)なら以降は計算しない
+
+        const $taskUrls = $('body').find('thead a');
+        let $taskId = '';
+
+        $taskUrls.each((index) => {
+            if (index == $clickedRow) {
+                const $url = $taskUrls[index].pathname;
+                const $elements = $url.split('/');
+                const $length = $elements.length;
+
+                $taskId = $elements[$length - 1]; // 0-indexed
+            }
+        });
 
         const $standings = $(this).siblings('td');
         const $username = $standings.find('.username span').text();
